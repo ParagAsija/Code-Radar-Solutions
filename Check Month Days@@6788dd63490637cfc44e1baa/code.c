3557f3ce1370a5d538af84fbd7da3d0096f591e0
ParagAsija <asijaparag5@gmail.com>
2025-01-27 17:12:56 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -3,16 +3,43 @@
 int main(){
     int month;
     scanf("%d", &month);
-    if (month ==1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 ){
-        printf("31");
-    }
-    else if(month == 4 || month == 6 || month == 9 || month == 11){
-        printf("30");
-    }
-    else if(month==2){
-        printf("28");
-    }
-    else {
-        printf("Invalid month");
+    // if (month ==1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 ){
+    //     printf("31");
+    // }
+    // else if(month == 4 || month == 6 || month == 9 || month == 11){
+    //     printf("30");
+    // }
+    // else if(month==2){
+    //     printf("28");
+    // }
+    // else {
+    //     printf("Invalid month");
+    // }
+    switch(month){
+        case 1: printf("31");
+        break;
+        case 2: printf("28");
+        break;
+        case 3: printf("31");
+        break;
+        case 4: printf("30");
+        break;
+        case 5: printf("31");
+        break;
+        case 6: printf("30");
+        break;
+        case 7: printf("31");
+        break;
+        case 8: printf("31");
+        break;
+        case 9: printf("30");
+        break;
+        case 10: printf("31");
+        break;
+        case 11: printf("30");
+        break;
+        case 12: printf("31");
+        break;
+        default: printf("Invalid month");
     }
 }
