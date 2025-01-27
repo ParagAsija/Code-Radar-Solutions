de55319dad50b5523579d1b327c7ac7beaa62566
ParagAsija <asijaparag5@gmail.com>
2025-01-27 17:48:05 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
-
 int main(){
     int n;
     scanf("%d", &n);
+    int max_stars = (2*n)-1;
+    int case_stars = (2*i)-1;
+    int k = n-1;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=((2*n)-1);j+=2){
-            if((j<=(((2*n)-(j+1))/2)) || (j>=(((2*n)-1+j)/2))){
+        for(int j=1;j<=(2*n)-1;j++){
+            if(j<=(max_stars-case_stars)/2){
+                printf(" ");
+            }
+            else if(j>(max_stars+case_stars)/2){
                 printf(" ");
             }
             else{
                 printf("*");
             }
         }
-    printf("\n");
+        printf("\n");
     }
-    for(int i=n-1;i>=1;i--){
-        for(int j=1;j<=(2*n)-1;j+=2){
-            if((j<=(((2*n)-(1+j))/2)) || (j>=(((2*n)-1+j)/2))){
+    for(int i=n;i>1;i--){
+        for(int j = (2*k)-1; j>=1; j--){
+            if(j<=(max_stars-case_stars)/2){
+                printf(" ");
+            }
+            else if(j>(max_stars+case_stars)/2){
                 printf(" ");
             }
             else{
