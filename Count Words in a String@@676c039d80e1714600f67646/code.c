3acaaa670e81df5a14d43867a6c5b043a5f0a21d
Parag Asija <asijaparag5@gmail.com>
2025-03-08 18:36:38 +0530

    Solution to Count Words in a String

--- Count Words in a String@@676c039d80e1714600f67646/code.c
+++ Count Words in a String@@676c039d80e1714600f67646/code.c
@@ -1,51 +1,54 @@
-// #include <stdio.h>
-// #include <string.h>
-
-// int main(){
-//     char input[100];
-//     scanf("%s", &input);
-//     int words = 0;
-//     for(int i = 0; i<strlen(input); i++){
-//         if(input[i] = ' ' || input[i] == '\t'){
-//             words ++;
-//         }
-//     }
-//     printf("%d", words);
-// }
-
 #include <stdio.h>
 #include <string.h>
 
-// Function to count the number of words in a string
-int countWords(char str[]) {
-    int count = 0;
-    int length = strlen(str);
-
-    for (int i = 0; i < length; i++) {
-        if (str[i] == ' ' && str[i+1] != ' ' && str[i+1] != '\0') {
-            count++;
+int main(){
+    char input[100];
+    scanf("%s", &input);
+    int words = 0;
+    for(int i = 0; i<strlen(input); i++){
+        if(input[i] = ' ' && input[i+1]!=' ' && input!='/O'){
+            words ++;
         }
     }
-
-    // Increment count for the last word
-    if (length > 0 && str[0] != ' ') {
-        count++;
+    if(strlen(input)>0 && input[0]!=' '){
+        words ++;
     }
-
-    return count;
+    printf("%d", words);
 }
 
-int main() {
-    char str[100];
-    fgets(str, sizeof(str), stdin);
+// #include <stdio.h>
+// #include <string.h>
 
-    // Remove trailing newline character
-    if ((strlen(str) > 0) && (str[strlen(str) - 1] == '\n')) {
-        str[strlen(str) - 1] = '\0';
-    }
+// // Function to count the number of words in a string
+// int countWords(char str[]) {
+//     int count = 0;
+//     int length = strlen(str);
 
-    int wordCount = countWords(str);
-    printf("%d", wordCount );
+//     for (int i = 0; i < length; i++) {
+//         if (str[i] == ' ' && str[i+1] != ' ' && str[i+1] != '\0') {
+//             count++;
+//         }
+//     }
 
-    return 0;
-}
+//     // Increment count for the last word
+//     if (length > 0 && str[0] != ' ') {
+//         count++;
+//     }
+
+//     return count;
+// }
+
+// int main() {
+//     char str[100];
+//     fgets(str, sizeof(str), stdin);
+
+//     // Remove trailing newline character
+//     if ((strlen(str) > 0) && (str[strlen(str) - 1] == '\n')) {
+//         str[strlen(str) - 1] = '\0';
+//     }
+
+//     int wordCount = countWords(str);
+//     printf("%d", wordCount );
+
+//     return 0;
+// }
