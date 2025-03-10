8b861626b2a6dd0c32aac98a56d1aac92e8b1a08
Parag Asija <asijaparag5@gmail.com>
2025-03-10 18:44:36 +0530

    Solution to Check if Two Strings are Rotations of Each Other

--- Check if Two Strings are Rotations of Each Other@@67877c59ddaeab38c8ad25ae/code.c
+++ Check if Two Strings are Rotations of Each Other@@67877c59ddaeab38c8ad25ae/code.c
@@ -4,10 +4,10 @@
 int main(){
     int arr1[100], arr2[100];
     scanf("%s %s", &arr1, &arr2);
+    int out = 1;
     for(int i = strlen(arr1) - 1 ; i>=0; i--){
     int j = 0;
     arr1[j] = arr1[i];
-    int out = 1;
     if(arr1[j] != arr2[j]){
         out = 0;
         break;
