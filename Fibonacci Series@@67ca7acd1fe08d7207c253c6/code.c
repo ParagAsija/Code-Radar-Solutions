defd4465bfe3bd40262006edf60c2e8ff3c36ea8
Parag Asija <asijaparag5@gmail.com>
2025-03-10 17:45:04 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,11 @@
+void fibonacciSeries(int n){
+    int arr[n];
+    arr[0] = 0;
+    arr[1] = 1;
+    for(int i = 2; i<n; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+}
